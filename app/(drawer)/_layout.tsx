edcde215d43d9ee9983b9beffea4bc19255ff09e
NhuongDT <inftech.ts91@gmail.com>
2025-07-26 11:44:44 +0700

    test - first commit

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+export const Drawer = () => {
+  return (
+    <Stack>
+      <Stack.Screen></Stack.Screen>
+    </Stack>
+  );
+};
---
+++ app/(drawer)/customer/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+const CustomerPage = () => {
+  return (
+    <ThemedView style={{ backgroundColor: "yellow" }}>
+      <ThemedText>page customer</ThemedText>
+    </ThemedView>
+  );
+};
